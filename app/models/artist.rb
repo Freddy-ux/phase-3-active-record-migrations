71e3903406e737408d3f56141fa0b2928b9dca15
Freddy-ux <alfredmuleri569@gmail.com>
2023-06-05 14:37:23 +0300

    Done with Assignment

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,1 +1,14 @@
-# add the Artist class here
+class CreateArtists < ActiveRecord::Base
+    def change
+      create_table :artists do |t|
+        t.string :name
+        t.string :genre
+        t.integer :age
+        t.string :hometown
+        # the id column is gemerated automatically for every table! no need to specify it here.
+      end
+    end
+  end
+
+  class Artist < ActiveRecord::Base
+  end
---
+++ db/migrate/20230605113550_add_favorite_food_to_artist.rb
@@ -0,0 +1,4 @@
+class AddFavoriteFoodToArtist < ActiveRecord::Migration[6.1]
+  def change
+  end
+end
